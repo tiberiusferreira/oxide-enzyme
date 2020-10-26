87d3a9dfe9175741ef39171246b287e2b3f2cf3a
Tiberio Ferreira <tiberiusferreira@gmail.com>
2020-10-26 01:20:44 -0300

    very simple toy network working

--- compile_release.sh
+++ compile_release.sh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 set -e
 output_dir="./target/release/deps"
-output_file="oxide_enzyme.ll"
-output_dir_n_file=$output_dir/$output_file
+input_llvm_filename="oxide_enzyme.ll"
+output_llvm_filename="processed_oxide_enzyme.ll"
+output_dir_n_file=$output_dir/$input_llvm_filename
 rm -f "./target/release/deps/oxide_enzyme.ll"
 cargo rustc --release -- --emit=llvm-ir || true
 if [ ! -f $output_dir_n_file  ]; then
     echo "Compilation failed"
     exit 1
 fi
+echo "Copying!"
 cp $output_dir_n_file .
-opt ${output_dir}/oxide_enzyme.ll -load=LLVMEnzyme-11.dylib -enzyme -o $output_file -S
-clang -m64 $output_file -Wl,-dead_strip -nodefaultlibs -lSystem -lresolv -lc -lm $(find /Users/tiberiodarferreira/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/x86_64-apple-darwin/lib -name '*rlib') -o test.exec
+opt $input_llvm_filename -load=LLVMEnzyme-11.dylib -enzyme -o $output_llvm_filename -S
+clang -m64 $output_llvm_filename -Wl,-dead_strip -nodefaultlibs -lSystem -lresolv -lc -lm $(find /Users/tiberiodarferreira/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/x86_64-apple-darwin/lib -name '*rlib') -o test.exec
 ./test.exec
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,146 @@
+use std::cell::RefCell;
+
 extern {
     fn __enzyme_autodiff(_: usize, ...);
 }
-use ndarray::Array2;
-fn vec_op_test_raw(left_ptr: *mut f64, left_len: i64, right_ptr: *mut f64, right_len: i64) -> f64{
-    let mut a_slice = unsafe{
-        std::slice::from_raw_parts_mut(left_ptr, left_len as usize)
+
+/// Dummy implementation
+fn linear_layer(tensor: &Tensor, weights: &Tensor) -> Tensor{
+    multiply(tensor, weights)
+}
+
+/// Dummy implementation
+fn reduce_sum_layer(tensor: &Tensor) -> f64{
+    tensor.data.iter().sum()
+}
+
+/// Dummy implementation
+fn softmax(tensor: &Tensor) -> Tensor{
+    let sum_exp_all: f64 = tensor.data.iter().fold(0., |acc, d|{
+        acc + d.exp()
+    });
+    let mut out: Vec<f64> = vec![0.; tensor.data.len()];
+    for i in 0..tensor.data.len(){
+        out[i] = tensor.data[i].exp()/sum_exp_all;
+    }
+    // TODO -> crashes
+    // let exp_all: Vec<f64> = tensor.data.iter().map(|d|{
+    //     d.exp()/sum_exp_all
+    // }).collect();
+    Tensor{
+        data: out,
+        shape: tensor.shape.clone()
+    }
+}
+
+/// Dummy implementation
+fn cross_entropy(input: &Tensor, target: &Tensor) -> f64{
+    let mut out = 0.;
+    for i in 0..input.data.len(){
+        out = out - target.data[i]*input.data[i].log(10.);
+    }
+    out
+}
+// logits=[a , b]
+// probdist = [0.3, 0.7]
+// label = [1, 0]
+
+// - (1*log(0.3) + 0*log(0.7))
+
+
+/// Dummy implementation
+fn dummy_nn(input: *mut f64, input_len: i64, linear_weights: *mut f64, linear_weights_len: i64,
+            target: *mut f64, target_len: i64) -> f64{
+    let input_as_slice = unsafe{
+        std::slice::from_raw_parts_mut(input, input_len as usize)
     };
-    let mut b_slice = unsafe{
-        std::slice::from_raw_parts_mut(right_ptr, right_len as usize)
+    let weights_as_slice = unsafe{
+        std::slice::from_raw_parts_mut(linear_weights, linear_weights_len as usize)
     };
-    let arr_a = unsafe {
-        // unchecked to prevent exception code from being generated
-        ndarray::Array2::from_shape_vec_unchecked([2, 2], a_slice.to_vec())
+    let target_as_slice = unsafe{
+        std::slice::from_raw_parts_mut(target, target_len as usize)
     };
-    let arr_b = unsafe {
-        // unchecked to prevent exception code from being generated
-        ndarray::Array2::from_shape_vec_unchecked([2, 2], b_slice.to_vec())
+    // TODO
+    // input_as_slice.to_vec() crashes!
+    let mut input_vec = vec![0.; input_as_slice.len()];
+    for i in 0..input_as_slice.len(){
+        input_vec[i] = input_as_slice[i];
+    }
+    let mut weight_vec = vec![0.; weights_as_slice.len()];
+    for i in 0..weights_as_slice.len(){
+        weight_vec[i] = weights_as_slice[i];
+    }
+    let mut target_vec = vec![0.; target_as_slice.len()];
+    for i in 0..target_as_slice.len(){
+        target_vec[i] = target_as_slice[i];
+    }
+    let input_tensor = Tensor{
+        data: input_vec,
+        shape: [2, 2]
     };
-    let c = arr_a.dot(&arr_b);
-    let res = c.iter().sum();
-    res
+    let weights_tensor = Tensor{
+        data: weight_vec,
+        shape: [2, 2]
+    };
+    let target_tensor = Tensor{
+        data: target_vec,
+        shape: [2, 2]
+    };
+    let linear_out = linear_layer(&input_tensor, &weights_tensor);
+
+    let soft_out = softmax(&linear_out);
+    // println!("{:?}", soft_out); // todo this crashes!
+    cross_entropy(&soft_out, &target_tensor)
+}
+
+#[derive(Debug, Clone)]
+struct Tensor{
+    data: Vec<f64>,
+    shape: [usize; 2]
+}
+
+/// Multiplies Square 2D Tensors dummy implementation
+fn multiply(left: &Tensor, right: &Tensor) -> Tensor
+{
+    let mut res : Vec<f64> = vec![0.; (left.shape[0]*left.shape[1]) as usize];
+    for i in 0..left.shape[0] {
+        for j in 0..left.shape[0] {
+            res[i*left.shape[0] + j] = 0.;
+            for k in 0..left.shape[0]{
+                res[i*left.shape[0] + j] += left.data[i*left.shape[0] + k] * right.data[k*left.shape[0] + j];
+            }
+        }
+    }
+    Tensor{
+        data: res,
+        shape: [2, 2]
+    }
 }
 
 fn main() {
+    let mut input = vec![1., 2., 3., 4.];
+    let mut input_shadow = vec![0.; 4];
+    let mut weights = vec![1.; 4];
+    let mut weights_shadow = vec![0.; 4];
+    let mut target = vec![0., 0., 1., 0.];
+    let mut target_shadow = vec![0., 0., 0., 0.];
 
-    let mut a = vec![1., 2., 3., 4.];
-    let mut a_shadow = vec![0., 0., 0., 0.];
-    let mut b = vec![1., 2., 3., 4.];
-    let mut b_shadow = vec![0., 0., 0., 0.];
-    println!("{:?}", vec_op_test_raw(a.as_mut_ptr(), a.len() as i64,
-                                     b.as_mut_ptr(), b.len() as i64));
-
-    unsafe {
-        __enzyme_autodiff(vec_op_test_raw as usize,
-                          a.as_mut_ptr(), a_shadow.as_mut_ptr(), a_shadow.len() as i64,
-                          b.as_mut_ptr(), b_shadow.as_mut_ptr(), b_shadow.len() as i64);
+    for _i in 0..70{
+        let res = dummy_nn(input.as_mut_ptr(),
+                           input.len() as i64,
+                           weights.as_mut_ptr(),
+                           weights.len() as i64,
+                           target.as_mut_ptr(),
+                           target.len() as i64);
+        println!("{:?}", res);
+        unsafe {
+            __enzyme_autodiff(dummy_nn as usize,
+                              input.as_mut_ptr(), input_shadow.as_mut_ptr(), input.len() as i64,
+                              weights.as_mut_ptr(), weights_shadow.as_mut_ptr(), weights.len() as i64,
+                              target.as_mut_ptr(), target_shadow.as_mut_ptr(), target.len() as i64);
+        }
+        weights.iter_mut().zip(weights_shadow.iter()).for_each(|(weight, grad)|{
+            *weight = *weight - *grad*0.01;
+        });
     }
-    dbg!(a);
-    dbg!(a_shadow);
 }
